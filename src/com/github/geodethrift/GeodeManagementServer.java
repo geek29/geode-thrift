f22546ce03c172625877e11c68f887d68b2517c1
geek29 <tkhairnar@pivotal.io>
2015-08-26 09:27:05 +0530

    Added Gfsh Result Structs

--- src/com/github/geodethrift/GeodeManagementServer.java
+++ src/com/github/geodethrift/GeodeManagementServer.java
@@ -7,28 +7,27 @@ import com.gemstone.gemfire.management.cli.CommandService;
 import com.gemstone.gemfire.management.cli.Result;
 
 public class GeodeManagementServer implements GemfireManagement.Iface {
-	
+
 	private GemFireCacheImpl impl;
 	private CommandService service;
-	
-	public GeodeManagementServer(){
+
+	public GeodeManagementServer() {
 		impl = GemFireCacheImpl.getExisting();
 		service = CommandService.getUsableLocalCommandService();
 	}
 
 	@Override
 	public String execute(Command cmd) throws CommandException, TException {
-		try{
-		Result result = service.processCommand(cmd.command);
-		StringBuilder sb = new StringBuilder();
-		while(result.hasNextLine()) {
-			sb.append(result.nextLine());
-		}
-		return sb.toString();
-		}catch(Exception e){
+		try {
+			Result result = service.processCommand(cmd.command);
+			StringBuilder sb = new StringBuilder();
+			while (result.hasNextLine()) {
+				sb.append(result.nextLine());
+			}
+			return sb.toString();
+		} catch (Exception e) {
 			throw new CommandException(cmd.command, e.getMessage());
 		}
 	}
-	
-	
+
 }
--- thrift/first.thrift
+++ thrift/first.thrift
@@ -20,7 +20,7 @@
  
 struct Command { 
  	1: string command
- 	2:map<string,string> env
+ 	2: map<string,string> env
 }
 
 exception CommandException {
@@ -30,4 +30,62 @@ exception CommandException {
 
 service GemfireManagement {
 	string execute(1:Command cmd) throws (1:CommandException ex)
+	
+	string getHelp(1:string command)
+}
+
+const string INFO_RESULT = "info"
+const string TABULAR_RESULT = "table"
+const string COMPOSITE_RESULT = "composite"
+const string ERROR_RESULT = "error"
+
+typedef list<String> Row
+
+struct InfoResult {
+	1: list<string> messages
+}
+
+struct TableResult {
+	1: list<string> colNames,
+	2: i16 rows,
+	3: i16 cols,
+	4: list<Row> data
+}
+
+struct Section {
+	1: string header,
+	2: list<Section> sections,
+	3: map<string,string> sectionMap,
+	4: list<TableResult> tables
+}
+
+struct CompositeResult {
+	1: list<string> sectionNames,
+	2: string header,
+	3: list<Section> sections
+}
+
+struct ErrorResult {
+	1: list<string> messages
+	2: i16 errorCode
+}
+
+struct ResultContent {
+	1: string header,
+	2: string footer,
+	3: string type,
+	4: optional InfoResult infoResult,
+	5: optional TableResult tableResult,
+	6: optional CompositeResult compositeResult,
+	7: optional ErrorResult errorResult
+}
+
+struct CommandResult {
+	1: string contentType,
+	2: i16 status,
+	3: string version,
+	4: string sender,
+	5: string debugInfo,
+	6: string when,
+	7: ResultContent content
 }
